e48e3c5bd0c10ce008e90bcc1c94b5cb99c1fac2
Your Name <irelvent@ex.com>
2023-05-03 23:03:43 +0300

    updates

--- sources/Team.hpp
+++ sources/Team.hpp
@@ -4,9 +4,32 @@
 #include "Character.hpp"
 #include "Cowboy.hpp"
 #include "Ninja.hpp"
+#include <vector>
 
 namespace ariel {
     class Team {
+        private:
+            Character leader;
+            vector<Character> champions;
+
+        public:
+            Team(Character leader) : leader(leader) {
+                this->champions.push_back(leader);
+            }
+
+            //destructor
+
+            // Insert a Character to a team
+            void add(Character* champion);
+
+            // edit
+            void attack(Team* enemy_team);
+
+            // Returns the amout of alive champions in team
+            int stillAlive();
+
+            // edit
+            void print();
 
     };
 }
