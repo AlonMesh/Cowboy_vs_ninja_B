9909e342b155a6a4f7908de4cad10f42315253dd
AlonMesh <alon3400@gmail.com>
2023-05-24 08:35:18 +0300

    SmartTeam func's and fixes

--- Demo.cpp
+++ Demo.cpp
@@ -23,96 +23,96 @@ using namespace ariel;
 */
 
 int main() {
-    // Point a(32.3,44),b(1.3,3.5);
-    // assert(a.distance(b) == b.distance(a));
-    // Cowboy *tom = new Cowboy("Tom", a);
-    // OldNinja *sushi = new OldNinja("sushi", b);
-    // tom->shoot(sushi);
-    // cout << tom->print() <<endl;
+    Point a(32.3,44),b(1.3,3.5);
+    assert(a.distance(b) == b.distance(a));
+    Cowboy *tom = new Cowboy("Tom", a);
+    OldNinja *sushi = new OldNinja("sushi", b);
+    tom->shoot(sushi);
+    cout << tom->print() <<endl;
 
-    // sushi->move(tom);
-    // sushi->slash(tom);
+    sushi->move(tom);
+    sushi->slash(tom);
 
-    // Team team_A(tom); 
-    // team_A.add(new YoungNinja("Yogi", Point(64,57)));
+    Team team_A(tom); 
+    team_A.add(new YoungNinja("Yogi", Point(64,57)));
 
-    // // Team b(tom); should throw tom is already in team a
+    // Team b(tom); should throw tom is already in team a
 
-    //  Team team_B(sushi);
-    //  team_B.add(new TrainedNinja("Hikari", Point(12,81)));
+     Team team_B(sushi);
+     team_B.add(new TrainedNinja("Hikari", Point(12,81)));
 
 
-    //  while(team_A.stillAlive() > 0 && team_B.stillAlive() > 0){
-    //     team_A.attack(&team_B);
-    //     team_B.attack(&team_A);
-    //     team_A.print();
-    //     team_B.print();
-    //  }
+     while(team_A.stillAlive() > 0 && team_B.stillAlive() > 0){
+        team_A.attack(&team_B);
+        team_B.attack(&team_A);
+        team_A.print();
+        team_B.print();
+     }
 
-    //  if (team_A.stillAlive() > 0) cout << "winner is team_A" << endl;
-    //  else cout << "winner is team_B" << endl;
+     if (team_A.stillAlive() > 0) cout << "winner is team_A" << endl;
+     else cout << "winner is team_B" << endl;
 
    //---- mine ------
-   random_device rd;
-   mt19937 gen(rd());
-   uniform_int_distribution<> distr_character(1, 10);
-   uniform_int_distribution<> distr_x(0, 1000);
-   uniform_int_distribution<> distr_y(0, 1000);
+   // random_device rd;
+   // mt19937 gen(rd());
+   // uniform_int_distribution<> distr_character(1, 10);
+   // uniform_int_distribution<> distr_x(0, 1000);
+   // uniform_int_distribution<> distr_y(0, 1000);
 
-   for (int i = 0 ; i < 400 ; i++) {
+   // for (int i = 0 ; i < 400 ; i++) {
 
-   Cowboy* cowboy_c = new Cowboy("LeaderC", Point(distr_x(gen), distr_y(gen)));
-   Team team_c(cowboy_c);
-   for (int i = 0; i < 9; i++) {
-      int random_num = distr_character(gen);
+   // Cowboy* cowboy_c = new Cowboy("LeaderC", Point(distr_x(gen), distr_y(gen)));
+   // Team team_c(cowboy_c);
+   // for (int i = 0; i < 9; i++) {
+   //    int random_num = distr_character(gen);
 
-      if (1 <= random_num && random_num <= 4) {
-         team_c.add(new Cowboy("cowboyC", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (5 <= random_num && random_num <= 6) {
-         team_c.add(new YoungNinja("YoungNinjaC", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (7 <= random_num && random_num <= 8) {
-         team_c.add(new TrainedNinja("TrainedNinjaC", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (9 <= random_num && random_num <= 10) {
-         team_c.add(new OldNinja("OldNinjaC", Point(distr_x(gen), distr_y(gen))));
-      }
-   }
+   //    if (1 <= random_num && random_num <= 4) {
+   //       team_c.add(new Cowboy("cowboyC", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (5 <= random_num && random_num <= 6) {
+   //       team_c.add(new YoungNinja("YoungNinjaC", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (7 <= random_num && random_num <= 8) {
+   //       team_c.add(new TrainedNinja("TrainedNinjaC", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (9 <= random_num && random_num <= 10) {
+   //       team_c.add(new OldNinja("OldNinjaC", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   // }
 
-   Cowboy* cowboy_d = new Cowboy("LeaderD", Point(distr_x(gen), distr_y(gen)));
-   Team team_d(cowboy_d);
-   for (int i = 0; i < 9; i++) {
-      int random_num = distr_character(gen);
+   // Cowboy* cowboy_d = new Cowboy("LeaderD", Point(distr_x(gen), distr_y(gen)));
+   // Team team_d(cowboy_d);
+   // for (int i = 0; i < 9; i++) {
+   //    int random_num = distr_character(gen);
 
-      if (1 <= random_num && random_num <= 4) {
-         team_d.add(new Cowboy("cowboyD", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (5 <= random_num && random_num <= 6) {
-         team_d.add(new YoungNinja("YoungNinjaD", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (7 <= random_num && random_num <= 8) {
-         team_d.add(new TrainedNinja("TrainedNinjaD", Point(distr_x(gen), distr_y(gen))));
-      }
-      if (9 <= random_num && random_num <= 10) {
-         team_d.add(new OldNinja("OldNinjaD", Point(distr_x(gen), distr_y(gen))));
-      }
-   }
+   //    if (1 <= random_num && random_num <= 4) {
+   //       team_d.add(new Cowboy("cowboyD", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (5 <= random_num && random_num <= 6) {
+   //       team_d.add(new YoungNinja("YoungNinjaD", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (7 <= random_num && random_num <= 8) {
+   //       team_d.add(new TrainedNinja("TrainedNinjaD", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   //    if (9 <= random_num && random_num <= 10) {
+   //       team_d.add(new OldNinja("OldNinjaD", Point(distr_x(gen), distr_y(gen))));
+   //    }
+   // }
 
-   while(team_c.stillAlive() > 0 && team_d.stillAlive() > 0){
-      team_c.attack(&team_d);
-      if (team_c.stillAlive() > 0 && team_d.stillAlive() > 0) {
-         team_d.attack(&team_c);
-      }
-      // team_A.print();
-      // team_B.print();
-   }
+   // while(team_c.stillAlive() > 0 && team_d.stillAlive() > 0){
+   //    team_c.attack(&team_d);
+   //    if (team_c.stillAlive() > 0 && team_d.stillAlive() > 0) {
+   //       team_d.attack(&team_c);
+   //    }
+   //    // team_A.print();
+   //    // team_B.print();
+   // }
 
-   for (auto member : team_c.getMembers()) {
-      cout << member->getName() << " dealt " << member->GetDmgDealt() << " dmg" << endl;
-   }
-   }
-   // >----mine-----
+   // for (auto member : team_c.getMembers()) {
+   //    cout << member->getName() << " dealt " << member->GetDmgDealt() << " dmg" << endl;
+   // }
+   // }
+   // // >----mine-----
      return 0; // no memory issues. Team should free the memory of its members. both a and b teams are on the stack. 
 
 }
--- sources/SmartTeam.hpp
+++ sources/SmartTeam.hpp
@@ -1,7 +1,7 @@
 #ifndef SMARTTEAM_HPP
 #define SMARTTEAM_HPP
 #include "Team.hpp"
-#include <algorithm>;
+#include <algorithm>
 
 namespace ariel {
     /**
@@ -12,8 +12,7 @@ namespace ariel {
     class SmartTeam : public Team
     {
     public:
-        SmartTeam(Character *leader) : Team(leader) {};
-
+        SmartTeam(Character *leader); 
         /**
          * I conducted an analysis to determine which character type dealt the most damage in a single round.
          * To achieve this, I created 400 random teams of 10 characters, with a distribution of:
@@ -32,208 +31,9 @@ namespace ariel {
          *      Old:     5.6593
          * (It basiclly means: for each 1 HP of a character, it deals this amount of damage)
         */
-        void attack(Team *enemy_team) override {
-
-            /**
-             * Before each attack, calculate the nubmer of alive cowboys and create alive ninja vector
-             * This will grant us quick information:
-             * 1. Instant damage (by cowboys)
-             * 2. Potential damage (by ninjas), limited by locations
-            */ 
-            int alive_cowboys_damage = 0;
-            vector<Character*> alive_ninjas;
-
-            for (auto member : this->getMembers()) {
-                if (member->isAlive()) {
-                    if (static_cast<Cowboy *>(member) != nullptr) {
-                        alive_cowboys_damage += 10;
-                    }
-                    else if 
-                    (static_cast<YoungNinja*>(member) != nullptr || 
-                     static_cast<TrainedNinja*>(member) != nullptr ||
-                     static_cast<OldNinja*>(member) != nullptr ) {
-                        alive_ninjas.push_back(member);
-                    }
-                }
-            }
-
-            // Collecting data: How much damage can be done against each enemy
-            vector<int> potential_damage_by_ninjas;
-            for (auto enemy : enemy_team->getMembers()) {
-                if (enemy->isAlive()) {
-                    int potential_damage_sum = 0;
-                    for (auto ninja : alive_ninjas) {
-                        if (ninja->distance(enemy) < 1) {
-                            potential_damage_sum += NINJA_DMG;
-                        }
-                    }
-                    potential_damage_by_ninjas.push_back(potential_damage_sum);
-                }
-
-                else { // enemy is dead
-                    // -1 is a flag to alert the program to skip that cell
-                    // It is cuz the i'th member will have the i'th element in potential_damage_by_ninjas
-                    potential_damage_by_ninjas.push_back(-1);
-                }
-            }
-
-            Character* target = findTarget(enemy_team->getMembers(), potential_damage_by_ninjas, alive_cowboys_damage);
-            
-            for (auto member : this->getMembers())
-            {
-                if (member->isAlive())
-                {
-                    if (dynamic_cast<Cowboy *>(member) != nullptr)
-                    {
-                        Cowboy *cowboy = dynamic_cast<Cowboy *>(member);
-                        if (cowboy->hasboolets())
-                        {
-                            cowboy->shoot(target);
-                            cowboy->raiseDmgDealt(COWBOY_DMG);
-                            alive_cowboys_damage -= 10;
-                        }
-                        else
-                        {
-                            cowboy->reload();
-                        }
-                    }
-                    else
-                    {
-                        if (dynamic_cast<YoungNinja*>(member) != nullptr) {
-                            YoungNinja *ninja = dynamic_cast<YoungNinja*>(member);
-                            ninja->actionControl(target);
-                            ninja->raiseDmgDealt(NINJA_DMG);
-                            
-                            // Remove it from the vector of the attacker ninjas
-                            auto it = std::find(alive_ninjas.begin(), alive_ninjas.end(), ninja);
-                            if (it != alive_ninjas.end()) {
-                                size_t index = std::distance(alive_ninjas.begin(), it);
-                                alive_ninjas.erase(alive_ninjas.begin() + index);
-                            }
-                        }
-                        else if (dynamic_cast<TrainedNinja*>(member) != nullptr) {
-                            TrainedNinja *ninja = dynamic_cast<TrainedNinja*>(member);
-                            ninja->actionControl(target);
-                            ninja->raiseDmgDealt(NINJA_DMG);
-
-                            // Remove it from the vector of the attacker ninjas
-                            auto it = std::find(alive_ninjas.begin(), alive_ninjas.end(), ninja);
-                            if (it != alive_ninjas.end()) {
-                                size_t index = std::distance(alive_ninjas.begin(), it);
-                                alive_ninjas.erase(alive_ninjas.begin() + index);
-                            }
-
-                        }
-                        else {
-                            OldNinja *ninja = dynamic_cast<OldNinja*>(member);
-                            ninja->actionControl(target);
-                            ninja->raiseDmgDealt(NINJA_DMG);
-
-                            // Remove it from the vector of the attacker ninjas
-                            auto it = std::find(alive_ninjas.begin(), alive_ninjas.end(), ninja);
-                            if (it != alive_ninjas.end()) {
-                                size_t index = std::distance(alive_ninjas.begin(), it);
-                                alive_ninjas.erase(alive_ninjas.begin() + index);
-                            }
-
-                        }
-                    }
-
-                    if (!target->isAlive()) {
-                        if (enemy_team->stillAlive() == 0) {
-                            break;
-                        }
-
-                        target = findTarget(enemy_team->getMembers(), potential_damage_by_ninjas, alive_cowboys_damage);                   
-                    }
-                }
-            }
-        }
+       void attack(Team *enemy_team) override;
         
-        Character* findTarget(vector<Character*> enemy_team, vector<int> potential_damage_by_ninjas, int potential_damage_by_cowboys) {
-            //TODO:
-                // dmg reading at the start of each findTarget...
-            for (size_t i = 0; i < enemy_team.size(); i++) {
-                auto enemy = enemy_team[i];
-                int potential_damage = potential_damage_by_ninjas[i] + potential_damage_by_cowboys;
-
-                if (enemy->isAlive()) {
-                    if (enemy->getHealthPoints() < potential_damage) {
-                        return enemy;
-                    }
-                }
-            }
-
-            /**
-             * If the program get into this part of the code, it means that
-             * the given target won't be killed this round.
-             * So the given target will be chosen by:
-             *      The enemy with the lowest hp by the next order:
-             *      YN, TN, OL, C
-            */
-            Character* target = nullptr;
-            int target_hp = YOUNG_NINJA_HP + 1;
-            for (auto enemy : enemy_team) {
-                if (enemy->isAlive()) {
-                    YoungNinja* yn = dynamic_cast<YoungNinja*>(enemy);
-                    if (yn != nullptr) {
-                        if (enemy->getHealthPoints() < target_hp) {
-                            target = yn;
-                            target_hp = enemy->getHealthPoints();
-                        }
-                    }
-                }
-            }
-            if (target != nullptr) {
-                return target;
-            }
-
-            target_hp = TRAINED_NINJA_HP + 1;
-            for (auto enemy : enemy_team) {
-                if (enemy->isAlive()) {
-                    TrainedNinja* tn = dynamic_cast<TrainedNinja*>(enemy);
-                    if (tn != nullptr) {
-                        if (enemy->getHealthPoints() < target_hp) {
-                            target = tn;
-                            target_hp = enemy->getHealthPoints();
-                        }
-                    }
-                }
-            }
-            if (target != nullptr) {
-                return target;
-            }
-
-            target_hp = OLD_NINJA_HP + 1;
-            for (auto enemy : enemy_team) {
-                if (enemy->isAlive()) {
-                    OldNinja* on = dynamic_cast<OldNinja*>(enemy);
-                    if (on != nullptr) {
-                        if (enemy->getHealthPoints() < target_hp) {
-                            target = on;
-                            target_hp = enemy->getHealthPoints();
-                        }
-                    }
-                }
-            }
-            if (target != nullptr) {
-                return target;
-            }
-
-            target_hp = COWBOY_HP + 1;
-            for (auto enemy : enemy_team) {
-                if (enemy->isAlive()) {
-                    Cowboy* cowboy = dynamic_cast<Cowboy*>(enemy);
-                    if (cowboy != nullptr) {
-                        if (enemy->getHealthPoints() < target_hp) {
-                            target = cowboy;
-                            target_hp = enemy->getHealthPoints();
-                        }
-                    }
-                }
-            }
-            return target;
-        }
+        Character* findTarget(vector<Character*> enemy_team, vector<int> potential_damage_by_ninjas, int potential_damage_by_cowboys);
     };
 }
 
